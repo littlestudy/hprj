471be8a1712b6f54fa22b091d6fcab080fde6296
littlestudy <includeio@163.com>
2015-03-03 14:14:07 +0800

    11

--- Avro/src/test/java/org/test/FileSystemTest.java
+++ Avro/src/test/java/org/test/FileSystemTest.java
@@ -2,10 +2,11 @@ package org.test;
 
 import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.io.MapWritable;
 
 public class FileSystemTest {
 
 	public static void main(String[] args) {
-		
+		MapWritable mw = new MapWritable();
 	}
 }
